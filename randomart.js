aaa8498c789438c46fbf91c76c4ab56e448f9d80
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2017-02-24 11:42:36 -0300

    Remove dependencies and use UMD
    Also fix whitespace.
    
    UMD module pattern from https://github.com/umdjs/umd/blob/master/templates/returnExports.js

--- randomart.js
+++ randomart.js
@@ -1,127 +1,135 @@
-(function() {
-
-var _ = require('lodash');
-var crypto = require('crypto');
-
-var defaultSymbols = {
-  "-2": "E", // end
-  "-1": "S", // start
-  "0": " ",
-  "1": ".",
-  "2": "o",
-  "3": "+",
-  "4": "=",
-  "5": "*",
-  "6": "B",
-  "7": "O",
-  "8": "X",
-  "9": "@",
-  "10": "%",
-  "11": "&",
-  "12": "#",
-  "13": "/",
-  "14": "^"
-};
-
-var special = {
-  end: -2,
-  start: -1,
-  empty: 0
-}
-
-var defaultBounds = {
-  width: 17,
-  height: 9
-};
-
-function createBoard(bounds, value) {
-  var result = [];
-
-  for (var i=0; i<bounds.width; i++) {
-    result[i] = [];
-    for (var j=0; j<bounds.height; j++) {
-      result[i][j] = value;
-    }
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
   }
+}(this, function() {
+  var defaultSymbols = {
+      "-2": "E", // end
+      "-1": "S", // start
+      "0": " ",
+      "1": ".",
+      "2": "o",
+      "3": "+",
+      "4": "=",
+      "5": "*",
+      "6": "B",
+      "7": "O",
+      "8": "X",
+      "9": "@",
+      "10": "%",
+      "11": "&",
+      "12": "#",
+      "13": "/",
+      "14": "^"
+    };
 
-  return result;
-}
+    var special = {
+      end: -2,
+      start: -1,
+      empty: 0
+    }
 
-function generateBoard(data, options) {
-  var options = options || {};
-  var bounds = options.bounds || defaultBounds;
+    var defaultBounds = {
+      width: 17,
+      height: 9
+    };
 
-  var board = createBoard(bounds, special.empty);
+    function createBoard(bounds, value) {
+      var result = [];
 
-  var x = Math.floor(bounds.width / 2);
-  var y = Math.floor(bounds.height / 2);
-
-  board[x][y] = special.start;
-
-  _.each(data, function(b) {
-    for (var s=0; s<8; s+=2) {
-      var d = (b >> s) & 3;
-
-      switch (d) {
-        case 0: // up
-        case 1:
-          if (y > 0) y--;
-          break;
-        case 2: // down
-        case 3:
-          if (y < (bounds.height - 1)) y++;
-          break;
-      }
-      switch (d) {
-        case 0: // left
-        case 2:
-          if (x > 0) x--;
-          break;
-        case 1: // right
-        case 3:
-          if (x < (bounds.width - 1)) x++;
-          break;
+      for (var i=0; i<bounds.width; i++) {
+        result[i] = [];
+        for (var j=0; j<bounds.height; j++) {
+          result[i][j] = value;
+        }
       }
 
-      if (board[x][y] >= special.empty) board[x][y]++;
+      return result;
     }
-  });
 
-  board[x][y] = special.end;
+    function generateBoard(data, options) {
+      var options = options || {};
+      var bounds = options.bounds || defaultBounds;
 
-  return {
-    board: board,
-    bounds: bounds
-  };
-}
+      var board = createBoard(bounds, special.empty);
 
-function boardToString(board, options) {
-  var options = options || {};
-  var symbols = options.symbols || defaultSymbols;
+      var x = Math.floor(bounds.width / 2);
+      var y = Math.floor(bounds.height / 2);
 
-  var width = board.bounds.width;
-  var height = board.bounds.height;
+      board[x][y] = special.start;
 
-  result = [];
+    data.forEach(function(b) {
+      for (var s=0; s<8; s+=2) {
+        var d = (b >> s) & 3;
 
-  for (var i=0; i<height; i++) {
-    result[i] = [];
-    for (var j=0; j<width; j++) {
-      result[i][j] = symbols[board.board[j][i]] || symbols[special.empty];
-    }
-    result[i] = result[i].join('');
+        switch (d) {
+          case 0: // up
+          case 1:
+            if (y > 0) y--;
+            break;
+          case 2: // down
+          case 3:
+            if (y < (bounds.height - 1)) y++;
+            break;
+        }
+        switch (d) {
+          case 0: // left
+          case 2:
+            if (x > 0) x--;
+            break;
+          case 1: // right
+          case 3:
+            if (x < (bounds.width - 1)) x++;
+            break;
+        }
+
+        if (board[x][y] >= special.empty) board[x][y]++;
+      }
+    });
+
+    board[x][y] = special.end;
+
+    return {
+      board: board,
+      bounds: bounds
+    };
   }
 
-  return result.join('\n');
-}
+  function boardToString(board, options) {
+    var options = options || {};
+    var symbols = options.symbols || defaultSymbols;
 
-function randomart(data, options) {
-  var data = data || crypto.pseudoRandomBytes(16);
-  var options = options || {};
+    var width = board.bounds.width;
+    var height = board.bounds.height;
 
-  return boardToString(generateBoard(data, options), options);
-}
+    result = [];
 
-module.exports = randomart;
+    for (var i=0; i<height; i++) {
+      result[i] = [];
+      for (var j=0; j<width; j++) {
+        result[i][j] = symbols[board.board[j][i]] || symbols[special.empty];
+      }
+      result[i] = result[i].join('');
+    }
 
-})();
+    return result.join('\n');
+  }
+
+  function randomart(data, options) {
+    if (!data) throw Error('missing data parameter');
+    var options = options || {};
+
+    return boardToString(generateBoard(data, options), options);
+  }
+
+  return randomart;
+});
